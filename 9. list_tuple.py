c5fcb32cb5d552980f53f426540d188bcae76231
John Maruf <marufakterjon@gmail.com>
2026-06-27 18:10:19 +0600

    Create 9. list_tuple.py

---
+++ 9. list_tuple.py
@@ -0,0 +1,21 @@
+a = [1, 2, 3, "john", " maruf", 4.5, 5.6, 6.7]
+# a[0] = 100
+# print(a)
+# print(a[-1])
+# print(len(a))
+
+# s = "hello"
+# print(list(s))
+
+
+a.append([1, 2, 3])
+print(a)
+print(a.index("john"))
+a.reverse()
+print(a)
+
+# tuple
+t = (1, 2, 3)
+t_r = reversed(t)
+print(t)
+print(t_r)
