714c1f37b22ec3289805865f3b1a12c31f907b52
John Maruf <marufakterjon@gmail.com>
2026-06-28 20:31:27 +0600

    Create 11. for.py

---
+++ 11. for.py
@@ -0,0 +1,16 @@
+# a = [1, 2, 3, 4, 5]
+
+# # for item in a:
+# #     print(item)
+
+# # b = "hello"
+# # for item in b:
+# #     print(item)
+# # print("h" in "hello")
+
+
+for i in range(1, 1001):
+    print(i)
+for i in range(101):
+    print(f"nasa hacking..{i}%")
+    print("nasa hacked successfully")
