019d7ec72ed04d79813134e7649ec779ecf3e634
John Maruf <marufakterjon@gmail.com>
2026-06-29 20:28:18 +0600

    Create 19. function.py

---
+++ 19. function.py
@@ -0,0 +1,11 @@
+# function 2 types
+# 1. user defined function - programmer nijer moto kore ekta function banabe
+# 2. built-in function - python er moddhe already ase
+
+print("hello")
+username = input("Please enter your name: ")
+print(f"welcome {username*2}")
+
+
+mx = max(10, 20, 30, 40, 50)
+print(f"maximum number is: {mx}. {mx*3}")
