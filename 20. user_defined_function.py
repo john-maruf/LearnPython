be65b71acd0f547c46427af2f052dcd7931e4659
John Maruf <marufakterjon@gmail.com>
2026-06-30 16:04:11 +0600

    Create 20. user_defined_function.py

---
+++ 20. user_defined_function.py
@@ -0,0 +1,27 @@
+# no input , no return
+
+
+def my_function():  # function definition
+    a = 10
+    b = 12
+    print(a + b)
+
+
+my_function()  # function call
+
+
+# input , no return
+
+
+def add_two_numbers(a, b):
+    print(a + b)
+
+
+add_two_numbers(5, 7)
+add_two_numbers(10, 15)
+
+# input , return
+
+
+def multiply_two_numbers(a, b):
+    return a * b
