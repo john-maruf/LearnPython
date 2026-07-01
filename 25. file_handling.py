1bd67913fe5363feba2aaf3bb6ce951b7a57a923
John Maruf <marufakterjon@gmail.com>
2026-07-01 22:15:15 +0600

    Create 25. file_handling.py

---
+++ 25. file_handling.py
@@ -0,0 +1,10 @@
+# file = open("D:\\Python\\Module 2\\name.txt", "r")
+# content = file.read()
+# print(content)
+# file.close()
+
+
+with open("D:\\Python\\Module 2\\name.txt", "r") as f:
+
+    content = f.read()
+    print(content)
